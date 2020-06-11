b103d53c626ff9bc0b1939dc5f9ce493586a28db
Matteo Iannoni <miannoniii@gmail.com>
2020-06-10 23:34:38 -0300

    second setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 8; // velocidade do jogador
+    public float gravity = -9.8f; // valor da gravidade
+    public LayerMask groundMask;
+    CharacterController character;
+    Vector3 velocity;
+    bool isGrounded;
+    void Start()
+    {
+        character = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        // Verifica se encostando no chão (o centro do objeto deve ser na base)
+        isGrounded = Physics.CheckSphere(transform.position, 0.2f, groundMask);
+
+        // Se no chão e descendo, resetar velocidade
+        if (isGrounded && velocity.y < 0.0f)
+        {
+            velocity.y = -1.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Rotaciona personagem
+        transform.Rotate(0, x * speed * 10 * Time.deltaTime, 0);
+
+        // Move personagem
+        Vector3 move = transform.forward * z;
+        character.Move(move * Time.deltaTime * speed);
+
+        // Aplica gravidade no personagem
+        velocity.y += gravity * Time.deltaTime;
+        character.Move(velocity * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/TerrainScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScript : MonoBehaviour
+{
+    public Terrain t;
+    public GameObject player;
+    private float [,] grassHeight;
+    public float grassGrowthRate = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(t.terrainData.detailHeight);
+        //print(t.terrainData.detailWidth);
+        grassHeight = new float[t.terrainData.detailHeight, t.terrainData.detailWidth];
+        for (var y = 0; y < t.terrainData.detailHeight; y++)
+        {
+            for (var x = 0; x < t.terrainData.detailWidth; x++)
+            {
+                grassHeight[x, y] = 0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var map = t.terrainData.GetDetailLayer(0, 0,
+          t.terrainData.detailWidth, t.terrainData.detailHeight, 0);
+
+        // For each pixel in the detail map...
+        for (var y = 0; y < t.terrainData.detailHeight; y++)
+        {
+            for (var x = 0; x < t.terrainData.detailWidth; x++)
+            {
+                
+
+                if ((player.transform.position - new Vector3(x,0.5f,y)).magnitude < 4)
+                {
+                    print("Log");
+                    print(y);
+                    //print(player.transform.position);
+                    //print((player.transform.position - new Vector3(x, 0.5f, y)).magnitude);
+                    print(x);
+                    grassHeight[y, x] = 0;
+                } else if (grassHeight[y, x] <= 4)
+                {
+                    grassHeight[y, x] += grassGrowthRate*Time.deltaTime;
+                }
+
+                map[y, x] = (int) grassHeight[y, x];
+            }
+        }
+
+
+        // Assign the modified map back.
+        t.terrainData.SetDetailLayer(0, 0, 0, map);
+
+    }
+
+
+}
