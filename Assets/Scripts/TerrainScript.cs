5279af4a86c0afedc759f7d69a40782b0c5587b9
Matteo Iannoni <miannoniii@gmail.com>
2020-06-19 18:34:55 -0300

    terreno

--- Assets/Scripts/TerrainScript.cs
+++ Assets/Scripts/TerrainScript.cs
@@ -9,14 +9,10 @@ public class TerrainScript : MonoBehaviour
     private float [,] grassHeight;
     public float grassGrowthRate = 0.1f;
     private int [,] initialGrassHeight;
-    //public int grass_type = 1;
-    //PlayerController pc;
 
     // Start is called before the first frame update
     void Start()
     {
-        //print(t.terrainData.detailHeight);
-        //print(t.terrainData.detailWidth);
         var map = t.terrainData.GetDetailLayer(0, 0,
           t.terrainData.detailWidth, t.terrainData.detailHeight, 0);
         initialGrassHeight = map;
@@ -38,8 +34,6 @@ public class TerrainScript : MonoBehaviour
 
         var map = t.terrainData.GetDetailLayer(0, 0,
           t.terrainData.detailWidth, t.terrainData.detailHeight, 0);
-        //t.terrainData.detailResolution
-        //t.terrainData.size
 
         // For each pixel in the detail map...
         for (var y = 0; y < t.terrainData.detailHeight; y++)
@@ -50,21 +44,12 @@ public class TerrainScript : MonoBehaviour
 
                 if ((player.transform.position - new Vector3(x*(t.terrainData.size.x / t.terrainData.detailResolution), player.transform.position.y , y*(t.terrainData.size.z / t.terrainData.detailResolution))).magnitude < 2f)
                 {
-                    //print("Log");
-                    //print(x);
-                    //print(y);
-                    //print(player.transform.position);
-                    //print(player.transform.position);
-                    //print((player.transform.position - new Vector3(x, 0.5f, y)).magnitude);
                     
                     grass += grassHeight[y, x];
-
                     grassHeight[y, x] = 0;
-                    //grassHeight[y, x] = 4;
 
                 } else if (grassHeight[y, x] <= initialGrassHeight[x, y])
                 {
-                    //grassHeight[y, x] = 0;
                     grassHeight[y, x] += grassGrowthRate*Time.deltaTime; // funcao da grama
                 }
 
